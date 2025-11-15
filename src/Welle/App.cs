d448c6a25938fa9f77cec80d29c15b68fad6d8bd
cacticrown <nathan.kichler@outlook.com>
2025-11-15 14:25:59 +0100

    FNA and Lua setup

---
+++ src/Welle/App.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Welle;
+
+public class App : Game
+{
+    public string ProjectPath;
+
+    public App(string projectPath)
+    {
+        ProjectPath = projectPath;
+
+        GraphicsDeviceManager gdm = new GraphicsDeviceManager(this);
+
+        gdm.IsFullScreen = false;
+        gdm.SynchronizeWithVerticalRetrace = true;
+
+        Window.AllowUserResizing = true;
+    }
+
+    protected override void Initialize()
+    {
+        /* This is a nice place to start up the engine, after
+         * loading configuration stuff in the constructor
+         */
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        // Load textures, sounds, and so on in here...
+        base.LoadContent();
+    }
+
+    protected override void UnloadContent()
+    {
+        // Clean up after yourself!
+        base.UnloadContent();
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        // Run game logic in here. Do NOT render anything here!
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        // Render stuff in here. Do NOT run game logic in here!
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+        base.Draw(gameTime);
+    }
+}
---
+++ src/Welle/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Welle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string projectPath = args.Length > 0 ? args[0] : string.Empty;
+
+        App app = new App(projectPath);
+        app.Run();
+    }
+}
