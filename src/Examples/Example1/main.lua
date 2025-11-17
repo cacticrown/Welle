80097fb788b8cc03672591e967b971ced97a3317
cacticrown <nathan.kichler@outlook.com>
2025-11-17 10:04:47 +0100

    update example

--- src/Examples/Example1/main.lua
+++ src/Examples/Example1/main.lua
@@ -2,13 +2,28 @@
 function load()
     print("Hello from Welle!")
 
-    texture = welle.graphics.loadTexture("image.png")
-    print(texture.path)
+    player = {
+        texture = welle.graphics.loadTexture("image.png")
+        x = 100,
+        y = 100,
+        speed = 5
+    }
+
+    print(player.texture.path)
 end
 
 function update(dt)
     if welle.input.keyboard.isKeyDown("a") then
-       print("a is down")
+       player.x = player.x - player.speed
+    end
+    if welle.input.keyboard.isKeyDown("d") then
+       player.x = player.x + player.speed
+    end
+    if welle.input.keyboard.isKeyDown("w") then
+       player.y = player.y - player.speed
+    end
+    if welle.input.keyboard.isKeyDown("s") then
+       player.y = player.y + player.speed
     end
 end
 
@@ -16,6 +31,6 @@ function draw()
     welle.graphics.clear(0.5, 0.5, 0.5, 1)
 
     welle.graphics.beginDraw()
-    welle.graphics.draw(texture, 100, 100)
+    welle.graphics.draw(player.texture, player.x, player.y)
     welle.graphics.endDraw()
 end
